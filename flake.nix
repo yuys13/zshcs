aa42ea05ee7b0908ec6a508560ccc44f4633b4a2
yuys13 <yuys13@users.noreply.github.com>
2026-07-12 10:32:28 +0900

    feat: add nix build and run support with CI verification

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A devShell for zshcs";
+  description = "zshcs - Zsh LSP server";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -50,6 +50,37 @@
               };
               yamlfmt.enable = true;
             };
+          };
+
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            pname = "zshcs";
+            version = "0.1.0";
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+
+            buildInputs = [
+              pkgs.openssl
+            ];
+
+            nativeCheckInputs = [
+              pkgs.zsh
+            ];
+
+            # Disable check phase because the tests launch a zsh subprocess
+            # using zpty, which requires pty access not available in the Nix sandbox.
+            doCheck = false;
+          };
+
+          apps.default = {
+            type = "app";
+            program = "${config.packages.default}/bin/zshcs";
           };
 
           devShells.default = pkgs.mkShell {
